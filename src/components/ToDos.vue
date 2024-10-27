c4b008d1b48d41eab06295d229a547a6966dff59
Marco Linke <birneos@gmx.net>
2024-10-27 07:33:19 +0100

    final first version, add last style for todo-container and label

--- src/components/ToDos.vue
+++ src/components/ToDos.vue
@@ -1,3 +1,63 @@
+<template>
+    <div class="todo-container w3-white w3-card-4">                    
+        <!-- Simple header -->                                         
+        <div class="w3-container w3-black w3-margin-0
+            w3-bottombar w3-border-blue">
+            <h1>
+                <i class="fa-solid fa-clipboard-list"></i>
+                To-Do List
+            </h1>
+        </div>
+
+
+    <!-- User input // 3-->                                                
+    <div class="w3-container flex-container w3-light-gray w3-padding">
+        <input class="w3-input w3-border-0" type="text"
+            autofocus
+            v-model="_todo_text"
+            @keyup.enter="addToDo()"
+            placeholder="Type here your to-do item...">
+        <button class="w3-button w3-gray" @click="clearToDo()">
+            <i class="fa-solid fa-times"></i>
+        </button>
+        <button class="w3-button w3-blue" @click="addToDo()">
+            <i class="fa-solid fa-plus"></i>
+        </button>
+    </div>
+
+
+    <!-- List of pending items -->                                    <!-- //4 -->
+    <div class="w3-padding w3-blue">Pending ({{ _pending.length }})</div>
+    <div class="w3-padding" v-for="todo in _pending" :key="todo.id">
+        <label>
+            <input type="checkbox" v-model="todo.checked">
+            <span class="w3-margin-left">
+                {{ todo.text }}
+            </span>
+    </label>
+    </div>
+    <div class="w3-padding" v-show="_pending.length == 0">No tasks</div>
+    <!-- List of completed tasks -->                                 <!--  //5 -->
+    <div class="w3-padding w3-blue">Completed ({{ _done.length }})
+    </div>
+    <div class="w3-padding" v-for="todo in _done" :key="todo.id">
+        <label>
+            <input type="checkbox" v-model="todo.checked">
+            <span class="w3-margin-left">
+                {{ todo.text }}
+            </span>
+        </label>
+    </div>
+    <div class="w3-padding" v-show="_done.length == 0">              <!--  //6 -->
+    No tasks
+    </div>
+
+
+    </div>
+
+</template>
+
+
 <script setup>
 import { ref, computed } from "vue"                        //1
 const                                                      //2
@@ -20,5 +80,9 @@ function addToDo() {                                       //6
     }
 }
 </script>
-<template>
-</template>
+
+<style scoped>
+    .todo-container {max-width: 100%; min-width: 30rem;}
+    label {cursor: pointer; display: flex;}
+
+</style>
